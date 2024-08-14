441a42541a2d0781bf0169d03cd7051144b0c40e
OdeiGarcia <112554299+OdeiGarcia@users.noreply.github.com>
2024-08-14 13:57:29 +0200

    Add files via upload

---
+++ parser/parser.py
@@ -0,0 +1,91 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+    S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
+    AP -> Adj | Adj AP
+    NP -> N | Det NP | AP NP | N PP
+    PP -> P NP
+    VP -> V | V NP | V NP PP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    return [
+        word.lower() for word in nltk.word_tokenize(sentence)
+        if any(c.isalpha() for c in word)
+    ]
+
+
+
+def np_chunk(tree):
+    n_chunk = []
+    par_tree = nltk.tree.ParentedTree.convert(tree)
+
+    for subtree in par_tree.subtrees():
+        if subtree.label() == "N":
+            n_chunk.append(subtree.parent())
+
+    return n_chunk
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
